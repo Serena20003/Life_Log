02571bac5fc22fca4725c12c8ad8c79931819e40
Serena20003 <scli@usc.edu>
2024-04-08 18:20:50 -0700

    Initial Commit

---
+++ LiSerenaFinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiSerenaFinalProject
+//
+//  Created by Serena Li on 2024/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiSerenaFinalProject/LiSerenaFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiSerenaFinalProjectApp.swift
+//  LiSerenaFinalProject
+//
+//  Created by Serena Li on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct LiSerenaFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
